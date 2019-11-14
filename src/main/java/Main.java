15953d07be06a9cc69e7f4143c4be77c4370e17e
HubCodes <krlrhkstk12@gmail.com>
2019-11-14 12:57:15 +0900

    Add example connection method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, JDBC howto!");
+    public static void main(String[] args) throws Exception {
+        String user = args[0];
+        String password = args[1];
+
+        Connection connection = null;
+        Properties connectionProps = new Properties();
+
+        connectionProps.put("user", user);
+        connectionProps.put("password", password);
+
+        connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/howtojdbc", connectionProps);
+
+        System.out.println("Connection Established!");
     }
 }
